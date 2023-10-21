7df1f485b3914b0081a4595459637ea91a21b64c
andremoreirapdev <andre.moreirap.dev@gmail.com>
2023-10-21 18:22:45 -0300

    feat: add ProductItem and ProductList components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,35 @@
 import Image from "next/image";
 import Categories from "./components/categories";
+import { prismaClient } from "@/lib/prisma";
+import ProductList from "./components/product-list";
 
-export default function Home() {
+export default async function Home() {
+  //Busca os produtos com discountPercentage > 0 (Ofertas)
+  const dells = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
   return (
-    <div className="p-5">
+    <div>
       <Image
         src="/banner-home-01.png"
         height={0}
         width={0}
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
         sizes="100vm"
         alt="Até 55% de desconto esse mês"
       />
 
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
+      </div>
+
+      <div className="mt-8">
+        <ProductList products={dells} />
       </div>
     </div>
   );
