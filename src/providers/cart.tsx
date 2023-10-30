9a70a526af0db61b0e589ee289b84f3fcc22f23a
andremoreirapdev <andre.moreirap.dev@gmail.com>
2023-10-30 20:42:31 -0300

    feat: add loadings pages and fixed bug localStorage on Next

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -36,12 +36,16 @@ export const CartContext = createContext<ICartContext>({
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
-  const [products, setProducts] = useState<CartProduct[]>(
-    JSON.parse(localStorage.getItem("tech-store/cart-products") || "[]"),
-  );
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
   useEffect(() => {
-    localStorage.setItem("tech-store/cart-products", JSON.stringify(products));
+    setProducts(
+      JSON.parse(localStorage.getItem("@tech-store/cart-products") || "[]"),
+    );
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("@tech-store/cart-products", JSON.stringify(products));
   }, [products]);
 
   //Total do carrinho sem descontos
